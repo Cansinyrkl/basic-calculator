2c5951253254adf905d3085690686050c695e6ed
Cansın <ayurekl97@gmail.com>
2022-12-22 23:24:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, useState } from "react";
 function App() {
+  const [count, setCount] = useState(null);
+  const [count2, setCount2] = useState(null);
+  const [operator, setOperator] = useState("+");
+  const [result, setResult] = useState();
+
+  const handleChange = (e) => {
+    if (e.target.id === "count") {
+      return setCount(e.target.value);
+    } else if (e.target.id === "count2") {
+      return setCount2(e.target.value);
+    } else if (e.target.id === "operator") {
+      return setOperator(e.target.value);
+    }
+  };
+
+  useEffect(() => {
+    if (operator === "+") {
+      return setResult(Number(count) + Number(count2));
+    } else if (operator === "-") {
+      return setResult(count - count2);
+    } else if (operator === "*") {
+      return setResult(count * count2);
+    } else if (operator === "/") {
+      return setResult(count / count2);
+    }
+  }, [operator, count, count2]);
+  console.log(result);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="number" id="count" onChange={handleChange}></input>
+      <select name="example" id="operator" multiple onChange={handleChange}>
+        <option value="+" selected>
+          +
+        </option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input type="number" id="count2" onChange={handleChange}></input>
+      <div>{result}</div>
     </div>
   );
 }
